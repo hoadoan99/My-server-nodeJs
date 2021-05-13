fd00340d722027c586294dfd3b8b14cf932019b0
hoadoan99 <overwatch19999@gmail.com>
2021-05-13 09:30:48 +0700

    Req - Res

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/contact', (req,res) => {
+  res.send('Contact Me: overwatch17777@gmail.com');
+})
+app.listen(3000, function(){
+  console.log("Server started on port 3000");
+});
